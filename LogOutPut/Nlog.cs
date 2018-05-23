d73565745e4a8eb7644e70222b12f34cf8bc1585
颜秉锋 <514669734@qq.com>
2018-05-23 08:36:13 +0800

    颜秉锋
    初始版本

---
+++ LogOutPut/Nlog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace LogOutPut
+{
+    /// <summary>
+    /// NLog插件日志使用
+    /// </summary>
+
+    public class Nlog
+    {
+        /// <summary>
+        /// 获取Nlog实例
+        /// </summary>
+        private static readonly Logger nLog = LogManager.GetCurrentClassLogger();
+
+        public static void main()
+        {
+            nLog.Trace("Trace Message");
+            nLog.Debug("Debug Message");
+            nLog.Info("Info Message");
+            nLog.Error("Error Message");
+            nLog.Fatal("Fatal Message");
+        }
+
+        public static void Trace(string value) {
+            nLog.Trace(value);
+        }
+
+        public static void Debug(string value)
+        {
+            nLog.Trace(value);
+        }
+
+        public static void Info(string value)
+        {
+            nLog.Trace(value);
+        }
+
+        public static void Error(string value)
+        {
+            nLog.Trace(value);
+        }
+
+        public static void Fatal(string value)
+        {
+            nLog.Trace(value);
+        }
+    }
+}
